95dddc4127625ac43570e6a48922b969868e9b33
HY KIM <chopinoff@gmail.com>
2023-07-02 20:59:52 +0900

    [백준] 1158 요세푸스 문제 김현영

--- Chop(Kim Hyeon Yeong)/230702_BAEK_1158_요세푸스_문제.py
+++ Chop(Kim Hyeon Yeong)/230702_BAEK_1158_요세푸스_문제.py
@@ -1,5 +1,8 @@
 from collections import deque
 
+# pop(0) 썼을 때 시간 초과
+# popleft() 사용하고 시간 초과 해결
+
 file = open("input/230702_BAEK_1158_요세푸스_문제.txt")
 N, K = map(int, file.readline().rstrip().split())
 num_arr = deque([str(i) for i in range(1, N+1)])
