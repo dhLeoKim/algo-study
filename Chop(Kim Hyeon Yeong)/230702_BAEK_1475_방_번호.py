ec5659cfd00fd1b77c1362d386ea706c5e20d605
HY KIM <chopinoff@gmail.com>
2023-07-02 20:59:23 +0900

    [백준] 1475 방 번호 김현영

--- Chop(Kim Hyeon Yeong)/230702_BAEK_1475_방_번호.py
+++ Chop(Kim Hyeon Yeong)/230702_BAEK_1475_방_번호.py
@@ -8,10 +8,10 @@ cnt_arr = [0] * 9
 
 for c in char_arr:
     if c == "6" or c == "9":
-        cnt_arr[6] += 0.5
+        cnt_arr[6] += 0.5 # 6, 9는 두 개당 스티커 한 판 필요
     else:
         cnt_arr[int(c)] += 1
 
-ans = math.ceil(max(cnt_arr))
+ans = math.ceil(max(cnt_arr)) # 소수점 올림 처리
 
 print(ans)
