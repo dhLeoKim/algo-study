da42a06efc5bfe5eaf8e34fb676a9bc755db6af2
HY KIM <chopinoff@gmail.com>
2023-08-27 20:46:20 +0900

    week 09 Chop 문제 풀이

--- PS/Chop/week_08/BOJ_2239_스도쿠.py
+++ PS/Chop/week_08/BOJ_2239_스도쿠.py
@@ -2,5 +2,48 @@ from sys import stdin
 
 stdin = open("input/BOJ_2239_스도쿠.txt")
 
-sudoku = [list(map(int, stdin.readline().rstrip())) for _ in range(9)]
-ans = []
+sudoku = []
+for _ in range(9):
+    sudoku += list(map(int, stdin.readline().rstrip()))
+
+def check_num(idx):
+    row_s = idx-idx%9
+    col_s = idx%9
+    square_s = (row_s//27)*27+col_s
+    area=[]
+
+    for i in range(9):
+        area.append(row_s+i)
+        area.append(col_s+9*i)
+        area.append(square_s+((i//3)*9)+(i%3))
+    area_set = list(set(area))
+    
+    num_list = []
+    for n in range(1, 10):
+        flag = True
+        for a in area_set:
+            if sudoku[a] == n:
+                flag = False
+                break
+        if flag:
+            num_list.append(n)
+    
+    return num_list
+
+
+def dfs(idx):
+
+    if idx == 81:
+        for i in range(9):
+            print("".join(sudoku[i*9:i*9+8]))
+        return
+    
+    if sudoku[idx] == 0:
+        for n in check_num(idx):
+            sudoku[idx] = n
+            dfs(idx+1)
+            sudoku[idx] = 0
+    else: dfs(idx+1)
+
+
+dfs(0)
--- PS/Chop/week_09/BOJ_2457_공주님의_정원.py
+++ PS/Chop/week_09/BOJ_2457_공주님의_정원.py
@@ -1,14 +1,38 @@
+from sys import stdin
 
-# map과 함께 사용하기
-numbers = [1, 2, 3, 4]
-mapped_numbers = list(map(lambda x: x*2, numbers))
-print(mapped_numbers)
-# [2, 4, 6, 8]
+stdin = open("input/BOJ_2457_공주님의_정원.txt")
 
-# sort와 함께 사용하기
-students = [{"name": "chop", "grade": 10}, {
-    "name": "pop", "grade": 12}, {"name": "hop", "grade": 8}]
-# 각 원소의 "grade" 기준으로 오름차순 정렬
-sorted_arr = sorted(students, key=lambda x: x["grade"])
-print(sorted_arr)
-# [{"name": "hop", "grade": 8}, {"name": "chop", "grade": 10}, {"name": "pop", "grade": 12}]
+N = int(stdin.readline().rstrip())
+arr = [list(map(int, stdin.readline().rstrip().split())) for _ in range(N)]
+
+# 날짜 대소 비교를 위해 월,일을 숫자로 변환 ex) 1월 1일 > 101
+flowers = [[x[0]*100+x[1], x[2]*100+x[3]] for x in arr]
+flowers.sort(key=lambda x: x[0])
+
+def count_flowers():
+    period = [101, 301]
+    idx, cnt = 0, 0
+
+    while period[1] <= 1130:
+        temp_end = period[0]
+        for i in range(idx, N):
+        
+            if period[0] <= flowers[i][0] <= period[1]:
+                if flowers[i][1] > 1130:
+                    cnt += 1
+                    return cnt
+                if flowers[i][1] >= temp_end:
+                    temp_end = flowers[i][1]
+                idx = i+1
+            else:
+                break
+                
+        if temp_end <= period[1]:
+            return 0
+        else:
+            cnt += 1
+            period = [period[1], temp_end]
+
+    return cnt
+
+print(count_flowers())
